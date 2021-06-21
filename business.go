2c3c86602347d654844143c09ef5f8666c290514
Nurahmet <nurahmet.azhibek@gmail.com>
2021-06-22 01:51:38 +0600

    Dynamic web page parser

--- business.go
+++ business.go
@@ -24,83 +24,61 @@ type JsonData struct {
 func Parse() ([]JsonData, error) {
 	var jsonData []JsonData
 	var err error
-	for i := 0; i < 10; i++ {
-		htmlRes, err := getHtmlPage(i)
-		if err != nil {
-			log.Fatal(err)
-		}
-		doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlRes))
-		doc.Find("#exploits-table tbody tr").Each(func(j int, item *goquery.Selection) {
-			var data JsonData
-			item.Children().Each(func(j int, child *goquery.Selection) {
-				if j == 0 {
-					data.Date = child.Text()
-				}
-				if j == 4 {
-					data.Title = child.Children().Text()
-					codeLink, _ := child.Children().Attr("href")
-					data.Code = "https://exploit-db.com" + codeLink
-				}
-				if j == 5 {
-					data.Type = child.Children().Text()
-				}
-				if j == 6 {
-					data.Platform = child.Children().Text()
-				}
-				if j == 7 {
-					data.Author = child.Children().Text()
-				}
-			})
-			jsonData = append(jsonData, data)
-		})
+	htmlRes, err := getHtmlPage()
+	if err != nil {
+		log.Fatal(err)
 	}
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlRes))
+	doc.Find("#exploits-table tbody tr").Each(func(j int, item *goquery.Selection) {
+		var data JsonData
+		item.Children().Each(func(j int, child *goquery.Selection) {
+			if j == 0 {
+				data.Date = child.Text()
+			}
+			if j == 4 {
+				data.Title = child.Children().Text()
+				codeLink, _ := child.Children().Attr("href")
+				data.Code = "https://exploit-db.com" + codeLink
+			}
+			if j == 5 {
+				data.Type = child.Children().Text()
+			}
+			if j == 6 {
+				data.Platform = child.Children().Text()
+			}
+			if j == 7 {
+				data.Author = child.Children().Text()
+			}
+		})
+		jsonData = append(jsonData, data)
+	})
 	return jsonData, err
 }
 
-func getHtmlPage(i int) (string, error) {
+func getHtmlPage() (string, error) {
 	ctx, cancel := chromedp.NewContext(
 		context.Background(),
 		chromedp.WithLogf(log.Printf),
 	)
 	var htmlRes string
-	var err error
+	defer cancel()
 	ctx, cancel = context.WithTimeout(ctx, 10*time.Second)
 	defer cancel()
-	if i == 0 {
-		err = chromedp.Run(ctx,
-			emulation.SetUserAgentOverride("WebScraper 1.0"),
-			chromedp.Navigate(`https://exploit-db.com`),
-			chromedp.Sleep(time.Second),
-			chromedp.Click("verifiedCheck", chromedp.ByID),
-			chromedp.Sleep(time.Second),
-			chromedp.WaitVisible("tbody tr", chromedp.ByQuery),
-			chromedp.ActionFunc(func(ctx context.Context) error {
-				node, err := dom.GetDocument().Do(ctx)
-				if err != nil {
-					return err
-				}
-				htmlRes, err = dom.GetOuterHTML().WithNodeID(node.NodeID).Do(ctx)
+	err := chromedp.Run(ctx,
+		emulation.SetUserAgentOverride("WebScraper 1.0"),
+		chromedp.Navigate(`https://exploit-db.com`),
+		chromedp.Sleep(time.Second),
+		chromedp.Click("verifiedCheck", chromedp.ByID),
+		chromedp.Sleep(time.Second),
+		chromedp.WaitVisible("tbody tr", chromedp.ByQuery),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			node, err := dom.GetDocument().Do(ctx)
+			if err != nil {
 				return err
-			}),
-		)
-	}
-	if i != 0 {
-		err = chromedp.Run(ctx,
-			emulation.SetUserAgentOverride("WebScraper 1.0"),
-			chromedp.Navigate(`https://exploit-db.com`),
-			chromedp.Sleep(time.Second),
-			chromedp.Click("exploits-table_next", chromedp.ByID),
-			chromedp.Sleep(time.Second),
-			chromedp.WaitVisible("tbody tr", chromedp.ByQuery),
-			chromedp.ActionFunc(func(ctx context.Context) error {
-				node, err := dom.GetDocument().Do(ctx)
-				if err != nil {
-					return err
-				}
-				htmlRes, err = dom.GetOuterHTML().WithNodeID(node.NodeID).Do(ctx)
-				return err
-			}),
-		)
-	}
+			}
+			htmlRes, err = dom.GetOuterHTML().WithNodeID(node.NodeID).Do(ctx)
+			return err
+		}),
+	)
 	return htmlRes, err
 }
